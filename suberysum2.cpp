3bd64916782338255a7c5570a4428fa0f08a593c
Dalia Akter <117200227+dalia-akter926@users.noreply.github.com>
2023-05-19 21:46:19 +0600

    Add files via upload

---
+++ suberysum2.cpp
@@ -0,0 +1,41 @@
+ #include<bits/stdc++.h>
+using namespace std;
+#define ln '\n';
+typedef long long ll;
+
+int main()
+{
+
+
+    int n,x;
+    cin >>n>>x;
+
+    vector<int> values(n);
+
+    for(int i=0; i<n; i++)
+        cin >> values[i];
+
+
+    ll sum=0;
+
+    map<ll,int> seen;
+
+    seen[0]++;
+
+    ll cnt=0;
+
+    for(int i=0; i<n; i++)
+    {
+        sum+=values[i];
+
+        cnt+=seen[sum-x];
+
+        seen[sum]++;
+
+
+    }
+
+
+    cout<<cnt;
+
+}
